1579402f006edde020d2a118a71121ee0a848c28
George Korepanov <gkorepanov.gk@gmail.com>
2023-07-06 19:08:34 +0300

    Update trnslation notebook

--- llm_tools/ui_translator.py
+++ llm_tools/ui_translator.py
@@ -1,9 +1,6 @@
-from typing import List, Dict, Iterable, AsyncIterator
+from typing import List, Dict
 from iso639 import Lang
-import asyncio
-from tenacity import retry, stop_after_attempt, wait_fixed
 
-from llm_tools.generator import make_generator
 
 
 DEFAULT_LANGUAGES = tuple(
@@ -12,88 +9,38 @@ DEFAULT_LANGUAGES = tuple(
 
 
 system_task = "You are a professional translator who helps to translate UI texts"
-user_task_template = """I need you to translate the UI text into following languages: {languages}. Respond with a yaml formatted like
-{yaml_example}.
-Use multiline yaml format if needed. The English UI text you need to translate is:
-\"\"\"{text}\"\"\"{extra_prompt}"""
+user_task_template = """I need you to translate the UI text into following languages: {languages}. Respond with a yaml formatted in the following way:
+```yaml
+{yaml_example}
+```
+The UI text you need to translate is:
+```yaml
+{example}
+```
+Only respond with a raw yaml, omit any comments. Include only this languages into output: {language_codes}
+"""
 
 
-@retry(stop=stop_after_attempt(4), wait=wait_fixed(1))
-async def get_translation_yaml(
+
+def get_translation_yaml(
     text: str,
     languages: List[Lang],
-    extra_prompt: str = "",
-    stream: bool = True,
-) -> AsyncIterator[str]:
+) -> List[Dict[str, str]]:
     yaml_example = []
     for x in languages:
-        yaml_example.append(f"{x.pt1}: |\n  <text>")
+        yaml_example.append(f"{x.pt1}: |-\n  <text>")
 
     yaml_example = '\n'.join(yaml_example)
 
     user_task = user_task_template.format(
-        text=text,
+        example=text,
         yaml_example=yaml_example,
         languages=', '.join(x.name for x in languages),
-        extra_prompt=extra_prompt,
+        language_codes=', '.join(x.pt1 for x in languages),
     )
 
     messages = [
         {"role": "system", "content": system_task},
         {"role": "user", "content": user_task},
     ]
-    line = []
-    async for token in make_generator(messages, model='gpt-4', stream=stream):
-        yield token
-
-
-async def get_full_translation_yaml(
-    english_strings: Dict[str, str],
-    languages: Iterable[Lang] = DEFAULT_LANGUAGES,
-    extra_prompt: str = "",
-    stream: bool = True,
-) -> AsyncIterator[str]:
-    languages = list(languages)
-    for name, text in english_strings.items():
-        yield f"{name}:\n  "
-        async for token in get_translation_yaml(
-            text,
-            languages,
-            extra_prompt=extra_prompt,
-            stream=stream,
-        ):
-            lines = token.split('\n')
-            yield lines[0]
-            for line in lines[1:]:
-                yield '\n  ' + line
-        yield '\n'
-
-
-async def get_full_translation_yaml_parallel(
-    english_strings: Dict[str, str],
-    languages: Iterable[Lang] = DEFAULT_LANGUAGES,
-    extra_prompt: str = "",
-    stream: bool = True,
-) -> AsyncIterator[str]:
-    languages = list(languages)
-    async def _get_result(name: str, text: str) -> str:
-        result = ""
-        result += f"{name}:\n  "
-        async for token in get_translation_yaml(
-            text,
-            languages,
-            extra_prompt=extra_prompt,
-            stream=stream,
-        ):
-            lines = token.split('\n')
-            result += lines[0]
-            for line in lines[1:]:
-                result += '\n  ' + line
-        return result
-
-    # yield results as they come
-    tasks = []
-    for name, text in english_strings.items():
-        tasks.append(_get_result(name, text))
-    for task in asyncio.as_completed(tasks):
-        yield await task
+    return messages
