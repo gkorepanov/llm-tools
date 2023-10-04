11449e7ef1105cb5363afa0bc8f2d9174facee94
George Korepanov <gkorepanov.gk@gmail.com>
2023-10-04 19:11:19 +0300

    Update function for tokens calculation

--- llm_tools/tokens.py
+++ llm_tools/tokens.py
@@ -1,12 +1,16 @@
 from dataclasses import dataclass
 from typing import List, Dict
+import logging
+import tiktoken
 
-from tiktoken import encoding_for_model
+
 from llm_tools.chat_message import (
     OpenAIChatMessage,
     prepare_messages,
     convert_message_to_dict,
 )
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -18,7 +22,7 @@ class TokenExpense:
     @property
     def n_total_tokens(self) -> int:
         return self.n_input_tokens + self.n_output_tokens
-    
+
     def __add__(self, other: "TokenExpense") -> "TokenExpense":
         if other.model_name != self.model_name:
             raise ValueError("Cannot add TokenExpense objects with different model names")
@@ -27,27 +31,25 @@ class TokenExpense:
             n_input_tokens=self.n_input_tokens + other.n_input_tokens,
             n_output_tokens=self.n_output_tokens + other.n_output_tokens,
         )
-    
+
     def price_per_1e6_input_tokens(self) -> int:
         return {
             "gpt-3.5-turbo": 2,
-            "gpt-3.5-turbo-16k": 3,
             "gpt-4": 30,
         }[self.model_name]
-    
+
     def price_per_1e6_output_tokens(self) -> int:
         return {
             "gpt-3.5-turbo": 2,
-            "gpt-3.5-turbo-16k": 4,
             "gpt-4": 60,
         }[self.model_name]
-    
+
     def get_price_multiplied_by_1e6(self) -> int:
         return (
             self.price_per_1e6_input_tokens() * self.n_input_tokens
             + self.price_per_1e6_output_tokens() * self.n_output_tokens
         )
-    
+
     def get_price(self) -> float:
         return self.get_price_multiplied_by_1e6() / 1e6
 
@@ -58,7 +60,7 @@ class TokenExpenses:
 
     def __init__(self):
         self.expenses = {}
-    
+
     def add_expense(self, expense: TokenExpense):
         if expense.model_name in self.expenses:
             self.expenses[expense.model_name] += expense
@@ -72,10 +74,10 @@ class TokenExpenses:
         for expense in other.expenses.values():
             result.add_expense(expense)
         return result
-    
+
     def get_price_multiplied_by_1e6(self) -> int:
         return sum(expense.get_price_multiplied_by_1e6() for expense in self.expenses.values())
-    
+
     def get_price(self) -> float:
         return self.get_price_multiplied_by_1e6() / 1e6
 
@@ -86,32 +88,12 @@ def count_tokens_from_input_messages(
 ) -> int:
     if not messages:
         return 0
-    encoding = encoding_for_model(model_name)
     messages_typed = prepare_messages(messages)
     message_dicts = [convert_message_to_dict(x) for x in messages_typed]
-
-    if model_name == "gpt-3.5-turbo":
-        tokens_per_message = 4  # every message follows <im_start>{role/name}\n{content}<im_end>\n
-        tokens_per_name = -1  # if there's a name, the role is omitted
-    elif model_name == "gpt-3.5-turbo-16k":
-        tokens_per_message = 4 
-        tokens_per_name = -1
-    elif model_name == "gpt-4":
-        tokens_per_message = 3
-        tokens_per_name = 1
-    else:
-        raise ValueError(f"Unknown model: {model_name}")
-
-    n_input_tokens = 0
-    for message_dict in message_dicts:
-        n_input_tokens += tokens_per_message
-        for key, value in message_dict.items():
-            n_input_tokens += len(encoding.encode(value))
-            if key == "name":
-                n_input_tokens += tokens_per_name
-
-    n_input_tokens += 3
-    return n_input_tokens
+    return num_tokens_from_messages(
+        messages=message_dicts,
+        model=model_name,
+    )
 
 
 def count_tokens_from_output_text(
@@ -120,5 +102,47 @@ def count_tokens_from_output_text(
 ) -> int:
     if not text:
         return 0
-    encoding = encoding_for_model(model_name)
+    encoding = tiktoken.encoding_for_model(model_name)
     return len(encoding.encode(text))
+
+
+def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613"):
+    """Copied from https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb
+    """
+    try:
+        encoding = tiktoken.encoding_for_model(model)
+    except KeyError:
+        logger.debug("Warning: model not found. Using cl100k_base encoding.")
+        encoding = tiktoken.get_encoding("cl100k_base")
+    if model in {
+        "gpt-3.5-turbo-0613",
+        "gpt-3.5-turbo-16k-0613",
+        "gpt-4-0314",
+        "gpt-4-32k-0314",
+        "gpt-4-0613",
+        "gpt-4-32k-0613",
+        }:
+        tokens_per_message = 3
+        tokens_per_name = 1
+    elif model == "gpt-3.5-turbo-0301":
+        tokens_per_message = 4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
+        tokens_per_name = -1  # if there's a name, the role is omitted
+    elif "gpt-3.5-turbo" in model:
+        logger.debug("Warning: gpt-3.5-turbo may update over time. Returning num tokens assuming gpt-3.5-turbo-0613.")
+        return num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613")
+    elif "gpt-4" in model:
+        logger.debug("Warning: gpt-4 may update over time. Returning num tokens assuming gpt-4-0613.")
+        return num_tokens_from_messages(messages, model="gpt-4-0613")
+    else:
+        raise NotImplementedError(
+            f"""num_tokens_from_messages() is not implemented for model {model}. See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens."""
+        )
+    num_tokens = 0
+    for message in messages:
+        num_tokens += tokens_per_message
+        for key, value in message.items():
+            num_tokens += len(encoding.encode(value))
+            if key == "name":
+                num_tokens += tokens_per_name
+    num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
+    return num_tokens
