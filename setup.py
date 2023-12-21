c639f916afa9ba50bc2febd6725ceaf0186c928a
Karim Iskakov <kar.iskakov@gmail.com>
2023-12-21 15:03:58 +0300

    Fix setup

--- setup.py
+++ setup.py
@@ -4,7 +4,15 @@ import pathlib
 here = pathlib.Path(__file__).parent.resolve()
 
 long_description = (here / "README.md").read_text(encoding="utf-8")
-requirements = (here / "requirements.txt").read_text(encoding="utf-8").splitlines()
+
+requirements = []
+dependency_links = []
+with open(here / "requirements.txt", encoding="utf-8") as f:
+    for line in f:
+        if "git+" in line:
+            dependency_links.append(line.strip())
+        else:
+            requirements.append(line.strip())
 
 
 setup(
@@ -19,4 +27,5 @@ setup(
     packages=find_packages(),
     python_requires=">=3.7",
     install_requires=requirements,
+    dependency_links=dependency_links,
 )
